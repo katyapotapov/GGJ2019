9641331baf9aa0f5e3a84a1f219b2a3c7ae9e428
Apaar Madan <apaar_madan@hotmail.com>
2019-01-25 22:54:57 -0500

    Synchronization stuff

--- client/socket.js
+++ client/socket.js
@@ -3,3 +3,7 @@ let ws = new WebSocket("ws://localhost:8080");
 ws.onmessage = function(e) {
     handleMessage(JSON.parse(e.data));
 }
+
+function sendMessage(message) {
+    ws.send(JSON.stringify(message));
+}
