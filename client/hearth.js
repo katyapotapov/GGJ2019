000bce78a3ae414cd9b41b1c22ca7b29c61aaad2
m5bhagat <m5bhagat@uwaterloo.ca>
2019-01-26 17:36:40 -0500

    Got the hearth working

--- client/hearth.js
+++ client/hearth.js
@@ -1,16 +1,30 @@
-HEARTH = {
+const HEARTH = {
     sprite: null,
     x: 0,
     y: 0,
     health: 20,
 }
 
-function initHearth() {
+function initHearth(x, y) {
     loadImage("assets/hearth.png", function (image) {
-        HEARTH.sprite = image;
+        HEARTH.sprite = createSprite({
+            image: image,
+            frameWidth: 64,
+            frameHeight: 64,
+
+            anims: {
+                crackle: {
+                    startFrame: 0,
+                    length: 5,
+                    frameTime: 0.1
+                }
+            },
+        });
+
+        HEARTH.sprite.x = x;
+        HEARTH.sprite.y = y;
+        HEARTH.x = x;
+        HEARTH.y = y;
+        playAnim(HEARTH.sprite, "crackle");
     });
-}
-
-function drawHearth() {
-
 }
