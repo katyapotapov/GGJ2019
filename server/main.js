04a88933d4c464e26710e61603a6d984398e9930
Apaar Madan <apaar_madan@hotmail.com>
2019-01-26 01:16:53 -0500

    Delete players on disconnect

--- server/main.js
+++ server/main.js
@@ -13,33 +13,49 @@ function handleDisconnection(ws) {
 
     connections.splice(i, 1);
 
+    for(let i = 0; i < connections.length; ++i) {
+        if(connections[i].readyState != 1) {
+            continue;
+        } 
+
+        connections[i].send(JSON.stringify({
+            clientID: ws.id,
+            disconnect: true
+        }));
+    }
+
     console.log("Handled disconnect.");
     ws.terminate();  
 }
 
+let counter = 0;
+
 wss.on("connection", function(ws) {
     console.log("Connection!");
-    let id = connections.length;
+    let id = counter;
+    counter += 1;
 
     connections.push(ws);
     ws.send(JSON.stringify({
         init: {
-            host: id === 0,
+            host: id == 0,
             clientID: id
         }
     }));
+
+    ws.id = id;
 
     for(let i = 0; i < connections.length; i++) {
         if(i == connections.length - 1) {
             for(let k = 0; k < connections.length; k++) {
                 connections[i].send(JSON.stringify({
-                    clientID: k,
+                    clientID: connections[k].id,
                     player: true
                 }));
             }
-        } else {
+        } else if(connections[i].readyState == 1) {
             connections[i].send(JSON.stringify({
-                clientID: connections.length - 1,
+                clientID: ws.id,
                 player: true
             }));
         }
