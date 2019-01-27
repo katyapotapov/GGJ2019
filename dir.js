79ec26a4d6a228b67170fba5ad3e2dc0c5d55000
Katya Potapov <kat.p28@gmail.com>
2019-01-26 20:16:22 -0500

    string to direction

--- dir.js
+++ dir.js
@@ -2,3 +2,18 @@ const DIR_UP = 0;
 const DIR_DOWN = 1;
 const DIR_LEFT = 2;
 const DIR_RIGHT = 3;
+
+function stringToDirection(string) {
+    if (string === "down") {
+        return DIR_DOWN;
+    } else if (string === "up") {
+        return DIR_UP;
+    } else if (string === "left") {
+        return DIR_LEFT;
+    } else if (string === "right") {
+        return DIR_RIGHT;
+    } else {
+        throw `No match for direction string ${string}`;
+    }
+}
+
