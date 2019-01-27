bf24aa995b1fa69c3252d135ffa602177f99eb66
Apaar Madan <apaar_madan@hotmail.com>
2019-01-26 21:54:29 -0500

    Debug rects

---
+++ debugrects.js
@@ -0,0 +1,42 @@
+let debugRects = [];
+
+function createDebugRect(x, y, w, h, fill, duration) {
+    if(!duration) {
+        duration = -1;
+    }
+
+    let rect = {
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        fill: fill,
+        time: duration
+    };
+
+    debugRects.push(rect);
+    
+    return rect;
+}
+
+function removeDebugRect(index) {
+    debugRects.splice(index, 1);
+}
+
+function updateDebugRects() {
+    for(let i = 0; i < debugRects.length; ++i) {
+        if(debugRects.duration > 0) {
+            debugRects.duration -= SEC_PER_FRAME;
+            if(debugRects.duration <= 0) {
+                removeDebugRect(i);
+            }
+        }
+    }
+}
+
+function drawDebugRects(cam) {
+    for(let i = 0; i < debugRects.length; ++i) {
+        ctx.fillStyle = debugRects[i].fill;
+        ctx.fillRect(debugRects[i].x - cam.x, debugRects[i].y - cam.y, debugRects[i].w, debugRects.h);
+    }
+}
