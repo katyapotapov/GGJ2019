3b3cad5a6fc5413ce7e44b066f71b0ef92d08af3
Katya Potapov <kat.p28@gmail.com>
2019-01-27 12:52:59 -0500

    Revert "Make sprite base smaller (can travel between tiles)"
    This reverts commit 35a3b751549e9ee39a40194847bbf3e72bca9f41.

--- players.js
+++ players.js
@@ -39,9 +39,9 @@ function createPlayer(id, x, y) {
         sprite: null,
         rect: {
             x: 20,
-            y: 38,
+            y: 32,
             w: 24,
-            h: 24
+            h: 32
         },
         inventory: {
             selected: 0,
