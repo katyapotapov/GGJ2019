53c0bf32951510e68935945d68cac0483cc9a19e
Apaar Madan <apaar_madan@hotmail.com>
2019-01-27 03:09:38 -0500

    Increase tickrate, send player join messages after stting host etc

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ function updateGame() {
         updateBullets();
         updateBombs();
 
-        if (tickCount >= 2) {
+        if (tickCount >= 1) {
             sendPlayers();
             sendBullets();
             tickCount = 0;
