af45de8001aea5e3228a8329ffb7e54911796924
Vania <vasuward@edu.uwaterloo.ca>
2019-01-26 20:36:13 -0500

    change wall life

--- buildings.js
+++ buildings.js
@@ -7,12 +7,12 @@ const HOUSE_DOOR_Y = 30;
 
 function createBuilding(x, y, l, w, doorX, doorY) {
     for (let i = 0; i < l; i++) {
-        createWall(x + 32 * i, y, DIR_UP, 3);
-        createWall(x + 32 * i, y + 32 * w, DIR_DOWN, 3);
+        createWall(x + 32 * i, y, DIR_UP, 10);
+        createWall(x + 32 * i, y + 32 * w, DIR_DOWN, 10);
     }
     for (let j = 0; j <= w; j++) {
-        createWall(x, y + 32 * j, DIR_LEFT, 3);
-        createWall(x + 32 * l, y + 32 * j, DIR_RIGHT, 3);
+        createWall(x, y + 32 * j, DIR_LEFT, 10);
+        createWall(x + 32 * l, y + 32 * j, DIR_RIGHT, 10);
     }
 }
 
