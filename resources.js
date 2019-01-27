2e05facd8586b9a7c59c5b6c05cdfc5bc64775bb
m5bhagat <m5bhagat@uwaterloo.ca>
2019-01-26 21:29:47 -0500

    Added random resource generation

--- resources.js
+++ resources.js
@@ -2,7 +2,6 @@ let resources = [];
 const BROWN_TREE = 1;
 const GREEN_TREE = 2;
 const ROCK = 3;
-const DIRT = 4;
 const RESOURCE_IMAGES = {
     brownTree: null,
     greenTree: null,
@@ -10,9 +9,8 @@ const RESOURCE_IMAGES = {
 };
 const FORBIDDEN_X = HOUSE_X;
 const FORBIDDEN_Y = HOUSE_Y;
-const FORBIDDEN_WIDTH = HOUSE_BLOCKS_LEFT * TILE_WIDTH;
-const FORBIDDEN_HEIGHT = HOUSE_BLOCKS_DOWN * TILE_HEIGHT;
-
+const FORBIDDEN_WIDTH = HOUSE_BLOCKS_LEFT * TILE_SIZE;
+const FORBIDDEN_HEIGHT = HOUSE_BLOCKS_DOWN * TILE_SIZE;
 
 function initResources() {
     loadImage("assets/browntree.png", function (image) {
@@ -27,7 +25,10 @@ function initResources() {
 }
 
 function initDefaultResources() {
-    createResource(BROWN_TREE, 50, 50);
+    for (let i = 0; i < 1000; ++i) {
+        console.log("Gen resource");
+        generateRandResources();
+    }
 }
 
 function createResource(type, x, y) {
@@ -60,8 +61,53 @@ function createResource(type, x, y) {
     }
 }
 
-function generateResources(type) {
+function generateRandResources() {
+    const mapWidth = tileMap.width * TILE_SIZE;
+    const mapHeight = tileMap.height * TILE_SIZE;
 
+    let x = 0;
+    let y = 0;
+    let type = ROCK;
+    let width = 0;
+    let height = 0;
+
+    x = randomNumInRange(0, 9);
+    if (x < 3) {
+        x = randomNumInRange(0, 1);
+        if (x == 0) {
+            type = GREEN_TREE;
+        } else {
+            type = BROWN_TREE;
+        }
+    }
+    if (type == ROCK) {
+        width = 80;
+        height = 65;
+    } else {
+        width = 120;
+        height = 140;
+    }
+
+    for (let i = 0; i < 50; i++) {
+        let x = randomNumInRange(0, mapWidth);
+        let y = randomNumInRange(0, mapHeight);
+        console.log(x);
+        console.log(y);
+
+        if (x > FORBIDDEN_X && x < (FORBIDDEN_X + FORBIDDEN_WIDTH)) {
+            continue;
+        }
+        if (y > FORBIDDEN_Y && y < (FORBIDDEN_Y + FORBIDDEN_HEIGHT)) {
+            continue;
+        }
+        let objects = getObjectsInRect(x, y, width, height, walls, resources, players);
+        if (objects.length != 0) {
+            continue;
+        }
+        createResource(type, x, y);
+        console.log(resources);
+        return;
+    }
 }
 
 function removeResource(index) {
@@ -87,13 +133,11 @@ function drawResources(cam) {
     for (let i = 0; i < resources.length; ++i) {
         let resource = resources[i];
         if (resource.type == BROWN_TREE) {
-            ctx.drawImage(RESOURCE_IMAGES.greenTree, resource.x - cam.x, resource.y - cam.y);
+            ctx.drawImage(RESOURCE_IMAGES.brownTree, resource.x - cam.x, resource.y - cam.y);
         } else if (resource.type == GREEN_TREE) {
-
+            ctx.drawImage(RESOURCE_IMAGES.greenTree, resource.x - cam.x, resource.y - cam.y);
         } else if (resource.type == ROCK) {
-
-        } else if (resource.type == DIRT) {
-
+            ctx.drawImage(RESOURCE_IMAGES.rock, resource.x - cam.x, resource.y - cam.y);
         }
     }
 }
