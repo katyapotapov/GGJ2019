bffb1ae03e27520eedcd29218384ba172ffa08a9
Katya Potapov <kat.p28@gmail.com>
2019-01-28 00:37:38 -0500

    more magic wood

--- items.js
+++ items.js
@@ -5,8 +5,8 @@ const ITEM_WOOD = 3;
 const ITEM_MAGIC_WOOD = 4;
 const ITEM_IMAGES = [null, null, null, null, null];
 const MAX_AMT_OF_ITEMS = 20;
-let magicWoodDropCounterMin = 15;
-let magicWoodDropCounterMax = 20;
+let magicWoodDropCounterMin = 40;
+let magicWoodDropCounterMax = 50;
 
 let items = [];
 let magicWoodDropCounter = 0;
